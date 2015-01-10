7c1a8d370548ed2865dc414e56921534224becd8
Rui Mendes <ruidamendes@ua.pt>
2015-01-10 16:49:08 +0000

    Load dataset and subset it between the dates for this exercise

--- plot4.R
+++ plot4.R
@@ -3,3 +3,12 @@
 ## Project1
 ## January 2014
 
+## Load dataset (in my case the dataset is on the data folder)
+data <- read.table("data/household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?")
+
+## Check the first 6 rows of teh dataset
+#head(data)
+
+## Subset the original dataset from the dates 2007-02-01 and 2007-02-02.
+subsetData <- subset(data, Date == "1/2/2007" | Date == "2/2/2007")
+
