38eee8ff83caf13785c716bd95b46f06c43067e6
Rui Mendes <ruidamendes@ua.pt>
2015-01-10 16:14:50 +0000

    Created plot1.R file and load the original dataset

---
+++ plot1.R
@@ -0,0 +1,7 @@
+## Exploratory Data Analysys
+## Rui Mendes (ruidamendes@ua.pt)
+## Project1
+## January 2014
+
+## Load dataset (in my case the dataset is on the data folder)
+data <- read.table("data/household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?")
